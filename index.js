762bd6eda66c6be590cd76670a4ea6ccc7c7a3ba
EmersonMedina <emermedina877@gmail.com>
2024-10-28 20:43:11 -0600

    Intentando corregir error de CORS

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(cors({
 app.use((req, res, next) => {
   const apiKey = req.headers['x-api-key'];
   if (apiKey === process.env.API_KEY) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   } else {
     res.status(403).json({ error: 'Unauthorized' });
